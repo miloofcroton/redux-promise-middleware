898551bface4b342e1b76509ad24b7b2d2afb018
Jack <ajackofsometrades@gmail.com>
2018-11-28 22:10:42 -0800

    working on testing for if promise is passed in

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 
+export const store = {
+  dispatch: jest.fn(),
+  getState: jest.fn()
+};
 // export const isPromise = payload => payload && !!payload.then ? true : false;
-
 export const isPromise = payload => {
   if(Promise && Promise.resolve) return Promise.resolve(payload) == payload;
   else throw 'Promise not supported in your environment';
@@ -9,6 +12,15 @@ export const isPromise = payload => {
 export const thoonk = store => next => action => {
 
   if(!isPromise(action.payload)) return next(action);
-  // next(action);
+  store.dispatch({ type: 'LOAD_START' });
+  action.payload()
+    .then(result => {
+      store.dispatch({ type: 'PROMISE_ACTION', payload: result });
+      store.dispatch({ type: 'LOAD_END' });
+    })
+    .catch(err => {
+      store.dispatch({ type: 'LOAD_END' });
+      store.dispatch({ type: 'ERROR', payload: err });
+    });
 
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,17 @@
 import { isPromise, thoonk } from './index';
 
+// jest.mock('./index.js', () => ({
+//   store: {
+//     dispatch: ({ type }) => {
+//       if(type === 'PROMISE_ACTION') return Promise.resolve('Did promise');
+//       else if(type === 'LOAD_START') return 'Loading';
+//       else if(type === 'LOAD_END') return 'Done Loading';
+//       else if(type === 'ERROR') return 'Error action';
+//       else return Promise.reject('404');
+//     }
+//   }
+// }));
+
 describe('isPromise?', () => {
 
   test('it returns true if payload is promise', () => {
@@ -31,6 +43,33 @@ describe('thoonk, a popular addition to thunk', () => {
     thoonk(store)(next)(action);
     expect(next.mock.calls).toHaveLength(1);
     expect(actionFn.mock.calls).toHaveLength(0);
+  });
+
+  test('if a promise is passed in, call PROMISE_ACTION and LOAD_END', () => {
+    const promiseFn = jest.fn();
+    const promise = () => new Promise((resolve, reject) => {
+      promiseFn();
+      resolve('done');
+    });
+
+
+    // const store = {
+    //   dispatch: jest.fn(),
+    //   getState: jest.fn()
+    // };
+    // const next = jest.fn();
+    // const actionFn = jest.fn();
+    // const action = {
+    //   payload: actionFn
+    // };
+    // thoonk(store)(next)(action);
+    // expect(next.mock.calls).toHaveLength(1);
+    // expect(actionFn.mock.calls).toHaveLength(0);
+
+  });
+
+  test('if a promise errors, call LOAD_END and ERROR', () => {
+
   });
 
 });
