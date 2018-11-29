d0608fa946c47e00e7663d97e5e5726b2c7344dd
Jack <ajackofsometrades@gmail.com>
2018-11-28 18:08:15 -0800

    isPromise is working

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+// export const isPromise = payload => payload && !!payload.then ? true : false;
+
+export const isPromise = payload => {
+  if(Promise && Promise.resolve) return Promise.resolve(payload) == payload;
+  else throw 'Promise not supported in your environment';
+};
+
+export const thoonk = store => next => action => {
+
+
+
+
+};
--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,24 @@
-// import React from 'react';
+import { isPromise, thoonk } from './index';
 
-describe('memoize(fn, equality)', () => {
+describe('isPromise?', () => {
 
-  const strictEquals = (itemA, itemB) => itemA === itemB;
+  test('it returns true if payload is promise', () => {
+    const promise = () => new Promise((resolve, reject) => resolve('done'));
+    const test = isPromise(promise());
+    expect(test).toBe(true);
+  });
 
-  test('memoize returns a function', () => {
+  test('it returns false if payload is not promise', () => {
+    const nonPromise = jest.fn();
+    const test = isPromise(nonPromise());
+    expect(test).toBe(false);
+  });
+
+});
+
+describe('thoonk, a popular alternative to thunk', () => {
+
+  test('it works', () => {
 
   });
 
