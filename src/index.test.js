9ca89d87e687625a786d695180aa71afe9e8f7ad
Jack <ajackofsometrades@gmail.com>
2018-11-28 16:57:15 -0800

    a.....scaffolding we will go!

---
+++ src/index.test.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+describe('memoize(fn, equality)', () => {
+
+  const strictEquals = (itemA, itemB) => itemA === itemB;
+
+  test('memoize returns a function', () => {
+
+  });
+
+});
